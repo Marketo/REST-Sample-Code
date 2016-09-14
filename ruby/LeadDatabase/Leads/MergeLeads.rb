9e11722a053f0e7150b4bf58342cb89a8f5a23ab
David Everly <deverly@marketo.com>
2016-09-14 14:25:23 -0700

    Fixed a few compile errors

--- ruby/LeadDatabase/Leads/MergeLeads.rb
+++ ruby/LeadDatabase/Leads/MergeLeads.rb
@@ -5,7 +5,7 @@ require 'json'
 #Replace AAA-BBB-CCC with your Marketo instance
 marketo_instance = "https://AAA-BBB-CCC.mktorest.com"
 #Specify winning lead id
-leadId = 2234
+leadId = "2234"
 endpoint = "/rest/v1/leads/" + leadId + "/merge.json"
 #Replace with your access token
 auth_token =  "?access_token=" + "ac756f7a-d54d-41ac-8c3c-f2d2a39ee325:ab"
@@ -14,7 +14,7 @@ losing_lead = "&leadId=" + "1234"
 request_url = marketo_instance + endpoint + auth_token + losing_lead
 
 #Make request
-response = RestClient.post request_url
+response = RestClient.post request_url, nil, :content_type => :json, :accept => :json
 
 #Returns Marketo API response
 puts response
