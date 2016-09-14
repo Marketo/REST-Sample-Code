2125f2fd385bc94d3049cfd623d4942376325c17
David Everly <deverly@marketo.com>
2016-09-14 14:20:28 -0700

    Replace rest_client with rest\-client

--- ruby/Asset/Files/GetFiles.rb
+++ ruby/Asset/Files/GetFiles.rb
@@ -1,4 +1,4 @@
-require 'rest_client'
+require 'rest-client'
 require 'json'
 
 host = "CHANGE ME"
--- ruby/LeadDatabase/Leads/GetLeadsByFilterType.rb
+++ ruby/LeadDatabase/Leads/GetLeadsByFilterType.rb
@@ -1,4 +1,4 @@
-require 'rest_client'
+require 'rest-client'
 require 'json'
 
 #Build request URL
