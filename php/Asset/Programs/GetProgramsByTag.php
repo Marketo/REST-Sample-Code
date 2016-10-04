6f9bc143dcdb5d522ade898147e158c1f636192f
David Everly <deverly@marketo.com>
2016-10-04 11:34:01 -0700

    PHP sample code for REST endpoints

---
+++ php/Asset/Programs/GetProgramsByTag.php
@@ -0,0 +1,42 @@
+<?php
+/*
+   GetProgramsByTag.php
+
+   Marketo REST API Sample Code
+   Copyright (C) 2016 Marketo, Inc.
+
+   This software may be modified and distributed under the terms
+   of the MIT license.  See the LICENSE file for details.
+*/
+$program = new GetProgramsByTag();
+$program->tagType = "Presenter";
+$program->tagValue = "David";
+
+print_r($program->getData());
+
+class GetProgramsByTag{
+	private $host = "CHANGE ME";
+	private $clientId = "CHANGE ME";
+	private $clientSecret = "CHANGE ME";
+	
+	public $name;//name of the program to retrieve
+
+	public function getData(){
+		$url = $this->host . "/rest/asset/v1/program/byTag.json?tagType=" . this.tagType . "&tagValue=" . this.tagValue;
+		$ch = curl_init($url);
+		curl_setopt($ch,  CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('accept: application/json', "Authorization: Bearer " . $this->getToken()));
+		$response = curl_exec($ch);
+		return $response;
+	}
+
+	private function getToken(){
+		$ch = curl_init($this->host . "/identity/oauth/token?grant_type=client_credentials&client_id=" . $this->clientId . "&client_secret=" . $this->clientSecret);
+		curl_setopt($ch,  CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('accept: application/json',));
+		$response = json_decode(curl_exec($ch));
+		curl_close($ch);
+		$token = $response->access_token;
+		return $token;
+	}
+}
