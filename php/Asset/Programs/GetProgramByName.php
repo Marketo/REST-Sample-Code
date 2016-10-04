3b9f82a09c9cded007bba544ec14e96c13d6c455
David Everly <deverly@marketo.com>
2016-10-04 11:16:14 -0700

    Improved program name value

--- php/Asset/Programs/GetProgramByName.php
+++ php/Asset/Programs/GetProgramByName.php
@@ -9,7 +9,7 @@
    of the MIT license.  See the LICENSE file for details.
 */
 $program = new GetProgramByName();
-$program->name = "Program";
+$program->name = "Test Program Name";
 
 print_r($program->getData());
 
