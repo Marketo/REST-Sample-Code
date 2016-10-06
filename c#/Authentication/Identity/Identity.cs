16a0218504a479983f047bf5660883d4aae47536
David Everly <deverly@marketo.com>
2016-10-06 16:17:56 -0700

    Moved Identity.cs under Identity folder

---
+++ c#/Authentication/Identity/Identity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json; // Json.NET (http://www.newtonsoft.com/json)
+
+namespace Marketo
+{
+    public class Identity
+    {
+        public static void Main(string[] args)
+        {
+            String host = "CHANGE ME";
+            String clientId = "CHANGE ME";
+            String clientSecret = "CHANGE ME";
+
+            String url = string.Format("https://{0}/identity/oauth/token?grant_type=client_credentials&client_id={1}&client_secret={2}", host, clientId, clientSecret);
+            String accessToken = Identity.getAccessToken(url);
+            String message = String.Format("Access Token: {0}", accessToken);
+            Console.WriteLine(message);
+        }
+
+        public static String getAccessToken(String url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = "application/json";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream resStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(resStream);
+            String json = reader.ReadToEnd();
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            return parsedJson.access_token;
+        }
+    }
+}
+
+
