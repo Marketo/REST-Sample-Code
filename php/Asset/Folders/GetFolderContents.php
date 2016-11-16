209ca5e005a9d60f44658dea06a5ebcb945366fb
David Everly <deverly@marketo.com>
2016-11-15 16:48:15 -0800

    PHP sample code for REST endpoints

---
+++ php/Asset/Folders/GetFolderContents.php
@@ -0,0 +1,50 @@
+<?php
+/*
+   GetFolderContents.php
+
+   Marketo REST API Sample Code
+   Copyright (C) 2016 Marketo, Inc.
+
+   This software may be modified and distributed under the terms
+   of the MIT license.  See the LICENSE file for details.
+*/
+$folder = new GetFolderContents();
+$folder->type = "Program";
+$folder->id = 1017;
+print_r($folder->getData());
+
+class GetFolderContents{
+	private $host = "CHANGE ME";
+	private $clientId = "CHANGE ME";
+	private $clientSecret = "CHANGE ME";
+    
+    public $id; //id of folder to retrieve, required
+	public $type; //type of folder to retrieve "Folder" or "Program", required
+	public $offset; //integer offset for paging, default 0
+	public $maxReturn; //max number of results, default 20, max 200
+	
+	public function getData() {
+		$url = $this->host . "/rest/asset/v1/folder/" . $this->id . "/content.json?access_token=" . $this->getToken() . "&type=" . $this->type;
+		if(isset($this->offset)){
+			$url .= "&offset=" . $this->offset;
+		}
+		if(isset($this->maxReturn)){
+			$url .= "&maxReturn=" . $this->maxReturn;
+		}
+		$ch = curl_init($url);
+		curl_setopt($ch,  CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('accept: application/json',));
+		$response = curl_exec($ch);
+		return $response;
+	}
+	
+	private function getToken() {
+		$ch = curl_init($this->host . "/identity/oauth/token?grant_type=client_credentials&client_id=" . $this->clientId . "&client_secret=" . $this->clientSecret);
+		curl_setopt($ch,  CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('accept: application/json',));
+		$response = json_decode(curl_exec($ch));
+		curl_close($ch);
+		$token = $response->access_token;
+		return $token;
+	}
+}
